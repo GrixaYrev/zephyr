e4eaf240ba03b8231cf501e02493fc2138c25449
YuryevGN <yuryev@strim-tech.com>
2021-05-07 18:13:27 +0300

    FlexIO SPI: init no HAL

--- drivers/spi/spi_mcux_flexio_spi.c
+++ drivers/spi/spi_mcux_flexio_spi.c
@@ -118,6 +118,101 @@ static void spi_mcux_master_transfer_callback(FLEXIO_SPI_Type *flexio_spi,
 }
 
 
+// static int spi_flexio_master_init(FLEXIO_SPI_Type *base, 
+// 																	uint8_t pol, uint8_t pha, flexio_spi_data_bitcount_mode_t mode,
+// 																	uint32_t src_clock, uint32_t baudrate)
+// {
+// 	flexio_shifter_config_t shifterConfig;
+//   flexio_timer_config_t timerConfig;
+// 	uint32_t ctrlReg  = 0;
+// 	uint16_t timerDiv = 0;
+// 	uint16_t timerCmp = 0;
+	
+// 	/* Clear the shifterConfig & timerConfig struct. */
+// 	(void)memset(&shifterConfig, 0, sizeof(shifterConfig));
+// 	(void)memset(&timerConfig, 0, sizeof(timerConfig));
+
+// #if !(defined(FSL_SDK_DISABLE_DRIVER_CLOCK_CONTROL) && FSL_SDK_DISABLE_DRIVER_CLOCK_CONTROL)
+// 	/* Ungate flexio clock. */
+// 	CLOCK_EnableClock(s_flexioClocks[FLEXIO_SPI_GetInstance(base)]);
+// #endif /* FSL_SDK_DISABLE_DRIVER_CLOCK_CONTROL */
+
+// 	/* Configure FLEXIO SPI Master */
+// 	ctrlReg = base->flexioBase->CTRL;
+// 	ctrlReg &= ~(FLEXIO_CTRL_DOZEN_MASK | FLEXIO_CTRL_DBGE_MASK | FLEXIO_CTRL_FASTACC_MASK | FLEXIO_CTRL_FLEXEN_MASK);
+// 	ctrlReg |= (FLEXIO_CTRL_DBGE(true) | FLEXIO_CTRL_FASTACC(false) | FLEXIO_CTRL_FLEXEN(true));
+// 	ctrlReg |= FLEXIO_CTRL_DOZEN_MASK;
+// 	base->flexioBase->CTRL = ctrlReg;
+
+// 	/* Do hardware configuration. */
+// 	/* 1. Configure the shifter 0 for tx. */
+// 	shifterConfig.timerSelect = base->timerIndex[0];
+// 	shifterConfig.pinConfig   = kFLEXIO_PinConfigOutput;
+// 	shifterConfig.pinSelect   = base->SDOPinIndex;
+// 	shifterConfig.pinPolarity = kFLEXIO_PinActiveHigh;
+// 	shifterConfig.shifterMode = kFLEXIO_ShifterModeTransmit;
+// 	shifterConfig.inputSource = kFLEXIO_ShifterInputFromPin;
+// 	if (!pha)
+// 	{
+// 		shifterConfig.timerPolarity = kFLEXIO_ShifterTimerPolarityOnNegitive;
+// 		shifterConfig.shifterStop   = kFLEXIO_ShifterStopBitDisable;
+// 		shifterConfig.shifterStart  = kFLEXIO_ShifterStartBitDisabledLoadDataOnEnable;
+// 	}
+// 	else
+// 	{
+// 		shifterConfig.timerPolarity = kFLEXIO_ShifterTimerPolarityOnPositive;
+// 		shifterConfig.shifterStop   = kFLEXIO_ShifterStopBitLow;
+// 		shifterConfig.shifterStart  = kFLEXIO_ShifterStartBitDisabledLoadDataOnShift;
+// 	}
+// 	FLEXIO_SetShifterConfig(base->flexioBase, base->shifterIndex[0], &shifterConfig);
+
+// 	/* 2. Configure the shifter 1 for rx. */
+// 	shifterConfig.timerSelect  = base->timerIndex[0];
+// 	shifterConfig.pinConfig    = kFLEXIO_PinConfigOutputDisabled;
+// 	shifterConfig.pinSelect    = base->SDIPinIndex;
+// 	shifterConfig.pinPolarity  = kFLEXIO_PinActiveHigh;
+// 	shifterConfig.shifterMode  = kFLEXIO_ShifterModeReceive;
+// 	shifterConfig.inputSource  = kFLEXIO_ShifterInputFromPin;
+// 	shifterConfig.shifterStop  = kFLEXIO_ShifterStopBitDisable;
+// 	shifterConfig.shifterStart = kFLEXIO_ShifterStartBitDisabledLoadDataOnEnable;
+// 	if (!pha)
+// 	{
+// 		shifterConfig.timerPolarity = kFLEXIO_ShifterTimerPolarityOnPositive;
+// 	}
+// 	else
+// 	{
+// 		shifterConfig.timerPolarity = kFLEXIO_ShifterTimerPolarityOnNegitive;
+// 	}
+// 	FLEXIO_SetShifterConfig(base->flexioBase, base->shifterIndex[1], &shifterConfig);
+
+// 	/*3. Configure the timer 0 for SCK. */
+// 	timerConfig.triggerSelect   = FLEXIO_TIMER_TRIGGER_SEL_SHIFTnSTAT(base->shifterIndex[0]);
+// 	timerConfig.triggerPolarity = kFLEXIO_TimerTriggerPolarityActiveLow;
+// 	timerConfig.triggerSource   = kFLEXIO_TimerTriggerSourceInternal;
+// 	timerConfig.pinConfig       = kFLEXIO_PinConfigOutput;
+// 	timerConfig.pinSelect       = base->SCKPinIndex;
+// 	timerConfig.pinPolarity     = kFLEXIO_PinActiveHigh;
+// 	timerConfig.timerMode       = kFLEXIO_TimerModeDual8BitBaudBit;
+// 	timerConfig.timerOutput     = kFLEXIO_TimerOutputZeroNotAffectedByReset;
+// 	timerConfig.timerDecrement  = kFLEXIO_TimerDecSrcOnFlexIOClockShiftTimerOutput;
+// 	timerConfig.timerReset      = kFLEXIO_TimerResetNever;
+// 	timerConfig.timerDisable    = kFLEXIO_TimerDisableOnTimerCompare;
+// 	timerConfig.timerEnable     = kFLEXIO_TimerEnableOnTriggerHigh;
+// 	timerConfig.timerStop       = kFLEXIO_TimerStopBitEnableOnTimerDisable;
+// 	timerConfig.timerStart      = kFLEXIO_TimerStartBitEnabled;
+
+// 	timerDiv = (uint16_t)(src_clock / baudrate);
+// 	timerDiv = timerDiv / 2U - 1U;
+
+// 	timerCmp = ((uint16_t)mode * 2U - 1U) << 8U;
+// 	timerCmp |= timerDiv;
+
+// 	timerConfig.timerCompare = timerCmp;
+
+// 	FLEXIO_SetTimerConfig(base->flexioBase, base->timerIndex[0], &timerConfig);
+// }
+
+
 static int spi_mcux_configure(const struct device *dev,
 			      const struct spi_config *spi_cfg)
 {
